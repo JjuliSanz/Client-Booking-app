4a0d1176655355bcc41cee436cdd6d61f0ebd078
Julian Sanz <jjulisanz@gmail.com>
2023-06-16 14:48:38 -0300

    navbar login added

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -3,6 +3,7 @@ import "./login.css";
 import { AuthContext } from "../../context/AuthContext";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import Navbar from "../../components/navbar/Navbar";
 
 const Login = () => {
   const BACKEND_URL = "https://backend-booking-app.onrender.com/api";
@@ -13,7 +14,7 @@ const Login = () => {
 
   const { loading, error, dispatch } = useContext(AuthContext);
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
@@ -23,37 +24,40 @@ const Login = () => {
     e.preventDefault();
     dispatch({ type: "LOGIN_START" });
     try {
-      const res = await axios.post(`${BACKEND_URL}/auth/login` , credentials);
+      const res = await axios.post(`${BACKEND_URL}/auth/login`, credentials);
       dispatch({ type: "LOGIN_SUCCESS", payload: res.data.details });
-      navigate("/")
+      navigate("/");
     } catch (err) {
       dispatch({ type: "LOGIN_FAILURE", payload: err.response.data });
     }
   };
 
   return (
-    <div className="login">
-      <div className="lContainer">
-        <input
-          type="text"
-          className="lInput"
-          placeholder="username"
-          id="username"
-          onChange={handleChange}
-        />
-        <input
-          type="password"
-          className="lInput"
-          placeholder="password"
-          id="password"
-          onChange={handleChange}
-        />
-        <button disabled={loading} className="lButton" onClick={handleClick}>
-          Login
-        </button>
-        {error && <span>{error.message}</span>}
+    <>
+      <Navbar />
+      <div className="login">
+        <div className="lContainer">
+          <input
+            type="text"
+            className="lInput"
+            placeholder="username"
+            id="username"
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            className="lInput"
+            placeholder="password"
+            id="password"
+            onChange={handleChange}
+          />
+          <button disabled={loading} className="lButton" onClick={handleClick}>
+            Login
+          </button>
+          {error && <span>{error.message}</span>}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/pages/register/register.jsx
+++ src/pages/register/register.jsx
@@ -3,6 +3,7 @@ import "./register.css";
 import { AuthContext } from "../../context/AuthContext";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import Navbar from "../../components/navbar/Navbar";
 
 const Register = () => {
   const BACKEND_URL = "https://backend-booking-app.onrender.com/api";
@@ -70,82 +71,85 @@ const Register = () => {
   const { loading, error, dispatch } = useContext(AuthContext);
 
   return (
-    <div className="rContainer">
-      <form onSubmit={handleSubmit} className="rForm">
-        <label className="rForm-label">
-          Image:
-          <input
-            type="file"
-            name="img"
-            onChange={handleImageChange}
-            className="rInput"
-          />
-        </label>
-        <label className="rForm-label">
-          Username:
-          <input
-            type="text"
-            name="username"
-            value={formData.username}
-            onChange={handleChange}
-            className="rInput"
-          />
-        </label>
-        <label className="rForm-label">
-          Email:
-          <input
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            className="rInput"
-          />
-        </label>
-        <label className="rForm-label">
-          Password:
-          <input
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            className="rInput"
-          />
-        </label>
-        <label className="rForm-label">
-          Country:
-          <input
-            type="text"
-            name="country"
-            value={formData.country}
-            onChange={handleChange}
-            className="rInput"
-          />
-        </label>
-        <label className="rForm-label">
-          City:
-          <input
-            type="text"
-            name="city"
-            value={formData.city}
-            onChange={handleChange}
-            className="rInput"
-          />
-        </label>
-        <label className="rForm-label-phone">
-          Phone:
-          <input
-            type="tel"
-            name="phone"
-            value={formData.phone}
-            onChange={handleChange}
-            className="rInput"
-          />
-        </label>
-        <button className="rButton" type="submit">
-          Submit
-        </button>
-      </form>
-    </div>
+    <>
+      <Navbar />
+      <div className="rContainer">
+        <form onSubmit={handleSubmit} className="rForm">
+          <label className="rForm-label">
+            Image:
+            <input
+              type="file"
+              name="img"
+              onChange={handleImageChange}
+              className="rInput"
+            />
+          </label>
+          <label className="rForm-label">
+            Username:
+            <input
+              type="text"
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
+              className="rInput"
+            />
+          </label>
+          <label className="rForm-label">
+            Email:
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              className="rInput"
+            />
+          </label>
+          <label className="rForm-label">
+            Password:
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              className="rInput"
+            />
+          </label>
+          <label className="rForm-label">
+            Country:
+            <input
+              type="text"
+              name="country"
+              value={formData.country}
+              onChange={handleChange}
+              className="rInput"
+            />
+          </label>
+          <label className="rForm-label">
+            City:
+            <input
+              type="text"
+              name="city"
+              value={formData.city}
+              onChange={handleChange}
+              className="rInput"
+            />
+          </label>
+          <label className="rForm-label-phone">
+            Phone:
+            <input
+              type="tel"
+              name="phone"
+              value={formData.phone}
+              onChange={handleChange}
+              className="rInput"
+            />
+          </label>
+          <button className="rButton" type="submit">
+            Submit
+          </button>
+        </form>
+      </div>
+    </>
   );
 };
 
